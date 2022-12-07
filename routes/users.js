26c791559274426268071a4cb36c8e5dda2434db
set <peter55605@gmail.com>
2022-12-07 11:27:48 +0800

    fix: /users 增加log

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from '../lib/logger.js'
 let router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  logger.info(`origin_url: ${req.originalUrl}|ip: ${req.ip}|x-forwarded-for: ${req.get('x-forwarded-for')}`)
   res.send('respond with a resource');
 });
 
